7f863da83ed4b4a8aa3a63d85783ce87995a0d74
alexahurtado08 <ahurtadod@eafit.edu.co>
2025-05-22 13:33:52 -0500

    Home administrador

--- User/urls.py
+++ User/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('registro/', views.registro, name='registro'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('admin-home/', views.admin_home, name='admin_home'),
 ]
--- User/views.py
+++ User/views.py
@@ -5,27 +5,32 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from .forms import RegisterUserForm
 from .models import Perfil
+from django.contrib.auth.decorators import login_required
 
 def inicio(request):
     return render(request, 'inicio.html')
 
 
 def login_user(request):
-	if request.method == "POST":
-		username = request.POST['username']
-		password = request.POST['password']
-		user = authenticate(request, username=username, password=password)
-		if user is not None:
-			login(request, user)
-			messages.success(request, "¡Has iniciado sesión exitosamente!")
-			return redirect('home')
-		else:
-			messages.success(request, ("Hubo un error al iniciar sesión, intenta de nuevo."))	
-			return redirect('login')	
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
 
+        if user is not None:
+            login(request, user)
+            messages.success(request, "¡Has iniciado sesión exitosamente!")
 
-	else:
-		return render(request, 'login.html', {})
+            # Redirección condicional
+            if user.is_superuser:
+                return redirect('admin_home')  # Vista para el administrador
+            else:
+                return redirect('home')  # Vista para usuario normal
+        else:
+            messages.error(request, "Hubo un error al iniciar sesión, intenta de nuevo.")
+            return redirect('login')
+
+    return render(request, 'login.html')
 
 def logout_user(request):
 	django_logout(request)
@@ -44,4 +49,8 @@ def registro(request):
         form = RegisterUserForm()
 
     return render(request, 'registro.html', {'form': form})
+
+@login_required
+def admin_home(request):
+    return render(request, 'admin_home.html')
 
