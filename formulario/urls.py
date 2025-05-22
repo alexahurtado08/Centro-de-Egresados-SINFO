10ca572eb946908d3873ddb24ae99ce1854ab534
alexahurtado08 <ahurtadod@eafit.edu.co>
2025-05-22 13:45:25 -0500

    Descagar Base de Datos

--- formulario/urls.py
+++ formulario/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.formulario, name='formulario'),
     path('usuarios/', views.lista_datos_usuarios, name='lista_usuarios'),
+    path('exportar-excel/', views.exportar_excel, name='exportar_excel'),
 ]
--- formulario/views.py
+++ formulario/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, redirect
 from .forms import DatosUsuarioForm
 from .models import DatosUsuario
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from import_export.formats.base_formats import XLSX
+from .admin import DatosUsuarioResource
 
 @login_required
 def formulario(request):
@@ -27,3 +30,18 @@ def formulario(request):
 def lista_datos_usuarios(request):
     datos = DatosUsuario.objects.all()  # obtiene todos los registros
     return render(request, 'formulario/lista_datos_usuarios.html', {'datos': datos})
+
+
+
+
+
+def exportar_excel(request):
+    resource = DatosUsuarioResource()
+    dataset = resource.export()
+
+    xlsx_format = XLSX()
+    export_data = xlsx_format.export_data(dataset)
+
+    response = HttpResponse(export_data, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename="base_datos_usuarios.xlsx"'
+    return response
